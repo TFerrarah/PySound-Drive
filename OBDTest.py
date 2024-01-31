0222a036c39e853f9fb2d3ed63af1dad66bb7b48
T_Ferrarah <ferrara.tommaso@panettipitagora.edu.it>
2024-01-31 15:52:36 +0100

    misc testing file

--- OBDTest.py
+++ OBDTest.py
@@ -1,5 +1,26 @@
-import obd
+import os
+import json
 
-connection = obd.OBD('/dev/tty.OBDII', 9600, protocol=7, fast=False)
+json_base = {"pedal" : [-1,-1],"redline" : -1,"idle" : -1}
 
-print(connection.status())
+#   Create JSON if not present
+if not os.path.exists("car_ranges.json"):
+    with open("car_ranges.json", "w") as json_file:
+        json_file.write(json.dumps(json_base))
+
+with open("car_ranges.json", "r+") as json_file:
+    car_values = json.load(json_file)
+
+    # Here you can edit car_values attributes
+
+
+    # Write to file
+    json_file.seek(0)
+    json_file.write(json.dumps(car_values))
+    json_file.truncate()
+
+# Read file
+with open("car_ranges.json", "r") as json_file:
+    car_values = json.load(json_file)
+    
+    # Do stuff with car_values
