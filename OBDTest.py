c252ebd010ed9aca71f247df621a5fecbda45c83
T_Ferrarah <ferrara.tommaso@panettipitagora.edu.it>
2024-02-01 15:35:38 +0100

    Misc testing

--- OBDTest.py
+++ OBDTest.py
@@ -1,26 +1,21 @@
-import os
-import json
+import obd
 
-json_base = {"pedal" : [-1,-1],"redline" : -1,"idle" : -1}
+ports = obd.scan_serial()      # return list of valid USB or RF ports
+print(ports)
 
-#   Create JSON if not present
-if not os.path.exists("car_ranges.json"):
-    with open("car_ranges.json", "w") as json_file:
-        json_file.write(json.dumps(json_base))
+# Choose OBDII port
+while True:
+    try:
+        obd_port = int(input("Please choose your port [0-"+str(len(ports)-1)+"]"))
+        if 0 <= obd_port <= int(len(ports)):
+            break
+        else:
+            print("Port number is not valid. Please try again")
+    except ValueError:
+        print("Input not recognized. Try again")
 
-with open("car_ranges.json", "r+") as json_file:
-    car_values = json.load(json_file)
 
-    # Here you can edit car_values attributes
+# Create connection
+connection = obd.OBD(ports[obd_port], baudrate=115200, protocol="7", fast=False) # auto-connects to USB or RF port
 
-
-    # Write to file
-    json_file.seek(0)
-    json_file.write(json.dumps(car_values))
-    json_file.truncate()
-
-# Read file
-with open("car_ranges.json", "r") as json_file:
-    car_values = json.load(json_file)
-    
-    # Do stuff with car_values
+print(connection.status())
