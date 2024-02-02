13c56f75970827e336b98794d2e3f09cd2a65fbd
TFerrarah <forkiofficialtech@gmail.com>
2024-02-02 11:53:05 +0100

    Added Assetto Corsa (Comp and non-com) full support

--- OBDTest.py
+++ OBDTest.py
@@ -1,21 +1,11 @@
-import obd
-
-ports = obd.scan_serial()      # return list of valid USB or RF ports
-print(ports)
-
-# Choose OBDII port
-while True:
-    try:
-        obd_port = int(input("Please choose your port [0-"+str(len(ports)-1)+"]"))
-        if 0 <= obd_port <= int(len(ports)):
-            break
-        else:
-            print("Port number is not valid. Please try again")
-    except ValueError:
-        print("Input not recognized. Try again")
-
-
-# Create connection
-connection = obd.OBD(ports[obd_port], baudrate=115200, protocol="7", fast=False) # auto-connects to USB or RF port
-
-print(connection.status())
+import argparse
+ 
+ 
+# Initialize parser
+parser = argparse.ArgumentParser()
+# Adding source argument
+parser.add_argument("-s", "--Source", help = "[OBD, AssettoCorsa] Input data Source (Assetto Corsa Competizione also works)", default = "OBD")
+# Read arguments from command line
+args = parser.parse_args()
+if args.Source:
+    print("Mode: "+str(args.Source))
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 import time
 import json
+import argparse
 from numpy import average
 from AudioStreams import AudioStreams
 from OBDHandler import OBDHandler
+from AcHandler import AcHandler
 
 DEBUG = False
 VERBOSE = True
@@ -24,46 +26,102 @@ audio_components = [separated_audio_dir+file for file in os.listdir(separated_au
 # Manage car's values file
 json_base = {"pedal" : [-1,-1],"redline" : -1,"idle" : -1}
 
+CAR_INFO_FILENAME = "car_ranges.json"
+
 #   Create JSON if not present
-if not os.path.exists("car_ranges.json"):
-    with open("car_ranges.json", "w") as json_file:
+if not os.path.exists(CAR_INFO_FILENAME):
+    with open(CAR_INFO_FILENAME, "w") as json_file:
         json_file.write(json.dumps(json_base))
 
-handler = OBDHandler()
+# Initiate OBDHandler
 
-with open("car_ranges.json", "r+") as json_file:
-    car_values = json.load(json_file)
-    if car_values["idle"] < 0 or car_values["redline"] < 0 or car_values["pedal"] == [-1,-1]:
-        input("No PySound-Drive calibration data was found. Press ENTER when you're ready for PySound-Drive calibration...")
-        # Calibrate idle rpm
-        while True:
-            try:
-                car_values["idle"] = int(input("Please enter your car's idle RPM [e.g. 950, 1000]: "))
-                if 0 <= car_values["idle"] <= 15000:
-                    break
-                else:
-                    print("Idle RPM must be between 0 - 15000")
-            except ValueError:
-                print("Input not recognized. Try again")
+# Initialize parser
+parser = argparse.ArgumentParser()
+# Adding source argument
+parser.add_argument("-s", "--Source", help = "[OBD, AC (AssettoCorsa, ACC, OBDII and OBD2 are also valid)] Input data Source (Assetto Corsa Competizione also works)", default = "OBD")
+# Read arguments from command line
+args = parser.parse_args()
+# Valid sources
+VALID_SOURCES = ["OBD", "AssettoCorsa", "AC", "ACC", "OBDII", "OBD2"]
+if args.Source in VALID_SOURCES:
+    if args.Source in ["OBD", "OBDII", "OBD2"]:
+        handler = OBDHandler()
 
-        # Calibrate redline rpm
-        while True:
-            try:
-                car_values["redline"] = int(input("Please enter your car's redline RPM [e.g. 4500, 8000]: "))
-                if 0 <= car_values["redline"] <= 15000:
-                    break
-                else:
-                    print("Redline RPM must be between 0 - 15000")
-            except ValueError:
-                print("Input not recognized. Try again")
+        # Values Calibration
+        with open(CAR_INFO_FILENAME, "r+") as json_file:
+            car_values = json.load(json_file)
+            if car_values["idle"] < 0 or car_values["redline"] < 0 or car_values["pedal"] == [-1,-1]:
+                input("No PySound-Drive calibration data was found. Press ENTER when you're ready for PySound-Drive calibration...")
+                # Calibrate idle rpm
+                while True:
+                    try:
+                        car_values["idle"] = int(input("Please enter your car's idle RPM [e.g. 950, 1000]: "))
+                        if 0 <= car_values["idle"] <= 15000:
+                            break
+                        else:
+                            print("Idle RPM must be between 0 - 15000")
+                    except ValueError:
+                        print("Input not recognized. Try again")
 
-        # Calibrate pedal
-        car_values["pedal"] = handler.calibrate_pedal()
+                # Calibrate redline rpm
+                while True:
+                    try:
+                        car_values["redline"] = int(input("Please enter your car's redline RPM [e.g. 4500, 8000]: "))
+                        if 0 <= car_values["redline"] <= 15000:
+                            break
+                        else:
+                            print("Redline RPM must be between 0 - 15000")
+                    except ValueError:
+                        print("Input not recognized. Try again")
 
-        # Write to file
-        json_file.seek(0)
-        json_file.write(json.dumps(car_values))
-        json_file.truncate()
+                # Calibrate pedal
+                car_values["pedal"] = handler.calibrate_pedal()
+
+                # Write to file
+                json_file.seek(0)
+                json_file.write(json.dumps(car_values))
+                json_file.truncate()
+
+    elif args.Source in ["AssettoCorsa", "AC", "ACC"]:
+        handler = AcHandler()
+
+        # Assetto corsa calibration (No pedal)
+        with open(CAR_INFO_FILENAME, "r+") as json_file:
+            car_values = json.load(json_file)
+            if car_values["idle"] < 0 or car_values["redline"] < 0:
+                input("No PySound-Drive calibration data was found. Press ENTER when you're ready for PySound-Drive calibration...")
+                # Calibrate idle rpm
+                while True:
+                    try:
+                        car_values["idle"] = int(input("Please enter your car's idle RPM [e.g. 950, 1000]: "))
+                        if 0 <= car_values["idle"] <= 15000:
+                            break
+                        else:
+                            print("Idle RPM must be between 0 - 15000")
+                    except ValueError:
+                        print("Input not recognized. Try again")
+
+                # Calibrate redline rpm
+                while True:
+                    try:
+                        car_values["redline"] = int(input("Please enter your car's redline RPM [e.g. 4500, 8000]: "))
+                        if 0 <= car_values["redline"] <= 15000:
+                            break
+                        else:
+                            print("Redline RPM must be between 0 - 15000")
+                    except ValueError:
+                        print("Input not recognized. Try again")
+
+                # Write to file
+                json_file.seek(0)
+                json_file.write(json.dumps(car_values))
+                json_file.truncate()
+
+else:
+    print("Invalid source. Please use one of the following: "+str(VALID_SOURCES))
+    exit()
+
+
 
 loop = AudioStreams(audio_components) # ! THIS IS WHERE AUDIO STARTS PLAYING
 
