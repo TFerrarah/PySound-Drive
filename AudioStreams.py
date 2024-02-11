4e622e1f804e4f7b180795c681725c0e2306fb6e
T_Ferrarah <55454285+TFerrarah@users.noreply.github.com>
2024-02-11 16:57:26 +0100

    Drammatically lowered Audio delay

--- AudioStreams.py
+++ AudioStreams.py
@@ -15,16 +15,21 @@ class AudioStreams():
 
     def __init__(self, components) -> None:
         # Start audio streams
-        self.streams = []
+        self.streams = ""
         self.streams_ports = {}
         port = INITIAL_ZMQ_PORT
 
         for c in components:
             filename = os.path.splitext(os.path.basename(c))[0]
             print(filename +" -> "+ str(port))
-            self.streams.append(subprocess.Popen(create_audio_cmd(c, port), shell=True))
+            # self.streams.append(subprocess.Popen(create_audio_cmd(c, port), shell=True))
+            self.streams += create_audio_cmd(c, port) + " | "
             self.streams_ports[filename] = port
             port = port+1
+        
+        streams=streams[:-3] # Remove last separator from streams
+        # Start all streams
+        self.ffplay_process = subprocess.Popen(streams, shell=True)
 
         # Start zmq sockets
         self.context = zmq.Context()
@@ -34,8 +39,6 @@ class AudioStreams():
             self.sockets[str(p)] = self.context.socket(zmq.REQ)
             self.sockets[str(p)].connect("tcp://127.0.0.1:"+str(p))
 
-
-    
     def get_streams_ports(self):
         return self.streams_ports
 
@@ -48,5 +51,4 @@ class AudioStreams():
         self.sockets[str(port)].recv()
 
     def stop_streams(self):
-        for s in self.streams:
-            s.terminate()
+        self.ffplay_process.terminate()
